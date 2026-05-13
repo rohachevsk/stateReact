59c95ed06662529ea3b54feacedeb56719711c40
ivanr <ivanr@ROHACHEVSKYI>
2026-05-13 15:36:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isDarkTheme: false,
+  };
+
+  toggleTheme = () => {
+    this.setState((prevState) => ({
+      isDarkTheme: !prevState.isDarkTheme,
+    }));
+  };
+
+  render() {
+    const { isDarkTheme } = this.state;
+    const themeClass = isDarkTheme ? 'dark' : 'light';
+
+    return (
+      <div className={`App ${themeClass}`}>
+        <header className="header">
+          <h1>{isDarkTheme ? 'Темная тема' : 'Светлая тема'}</h1>
+          <button onClick={this.toggleTheme}>Сменить тему</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
